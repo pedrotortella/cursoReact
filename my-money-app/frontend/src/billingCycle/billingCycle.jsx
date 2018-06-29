67804482065bd6d1e6f6878b7445edaba15be141
Pedro Luiz Tortella <pl.tortella@samsung.com>
2018-06-29 10:56:41 -0300

    Add edit form

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -10,7 +10,7 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create } from './billingCycleActions'
+import { create, update } from './billingCycleActions'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
 
@@ -45,7 +45,7 @@ class BillingCycle extends Component {
                 <Form onSubmit={this.props.create} />
               </TabContent>
               <TabContent id='tabUpdate'>
-                <h1>Alterar</h1>
+                <Form onSubmit={this.props.update}/>
               </TabContent>
               <TabContent id='tabDelete'>
                 <h1>Excluir</h1>
@@ -57,5 +57,5 @@ class BillingCycle extends Component {
     )
   }
 }
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create, update }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
